b7c43d099d6cd6e89f0046a963fc4ff8569e885e
Gabiita <38547554+Gabiita@users.noreply.github.com>
2018-04-22 21:10:52 -0500

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingresa tu nombre");
+        String nombre = entrada.nextLine();
+        System.out.println("Tu nombre es:" + nombre);
+
+    }
+}
