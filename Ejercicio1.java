8abb48413ae148cb17c7e41997da8de88833dee2
Gabiita <38547554+Gabiita@users.noreply.github.com>
2018-04-22 21:07:01 -0500

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,10 @@
+
+public class Ejercicio1 {
+   public static void main(String [] args) {
+       double fahrenheit=80;
+       double celsius;
+       celsius=((fahrenheit-32)*5)/9;
+       System.out.println("Su temperatura es:");
+       System.out.println(celsius);
+   }
+}
